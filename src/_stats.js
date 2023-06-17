b5c4efb22275a3d64ddc17cffff8d5541034e024
hville <hugov@runbox.com>
2023-06-16 22:08:25 -0400

    stats resolution fix

--- src/_stats.js
+++ src/_stats.js
@@ -14,12 +14,13 @@ export default class Stats {
 		const dim = names.length,
 					lazyLength = (dim+1)*(dim+2)/2,
 					indexOf = Object.fromEntries( names.map( (n,i) => [n,i] ) ),
-					buffer = resolution instanceof ArrayBuffer ? resolution : new ArrayBuffer( (lazyLength + dim*resolution*2) * 64 )
+					buffer = resolution instanceof ArrayBuffer ? resolution : new ArrayBuffer( (lazyLength + dim*resolution*2) * 64 ),
+					res2 = Math.floor( (buffer.byteLength/64 - lazyLength)/dim )
 		let view = nextView(buffer, Float64Array, lazyLength)
 		const moments = new LazyStats( view )
 
 		for (let i=0; i<dim; ++i) {
-			view = nextView(view, Float64Array, resolution*2)
+			view = nextView(view, Float64Array, res2)
 			const stat = this[names[i]] = new SampleDistribution( view )
 			stat.ave = () => moments.ave( i )
 			stat.dev = () => moments.dev( i )
